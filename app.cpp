8e4387c3a912d332162effe9c63e1b2f6c90d853
Josué Obed Landaverde <129899297+jlndvr@users.noreply.github.com>
2025-05-08 14:47:06 -0500

    Create app.cpp

---
+++ app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+#include "profile.hpp"
+
+int main() {
+
+Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+sam.add_hobby("listening to audiobooks and podcasts");
+std::cout << sam.view_profile();
+}
